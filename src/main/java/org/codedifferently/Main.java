bd723880e10a3de26b760db00402a7e545d15a3a
mkbrown261 <aviatorkeyz@icloud.com>
2026-02-06 17:29:54 -0500

    Mason-Brown-reciept

--- src/main/java/org/codedifferently/Main.java
+++ src/main/java/org/codedifferently/Main.java
@@ -1,17 +1,21 @@
 package org.codedifferently;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main {
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Welcome to Durleys Pharmacy!");
+       System.out.println("What is your name");
+       String name = scanner.nextLine();
+       System.out.println("What is your budget:");
+       double budget = scanner.nextDouble();
+       System.out.println("Enter your 5 digit rewards number");
+       double rewardnumber = scanner.nextInt();
+       recieptcalculator recieptcalculator = new recieptcalculator();
+       System.out.println(recieptcalculator.generatethereceipt());
+
+
     }
+
 }
---
+++ src/main/java/org/codedifferently/recieptcalculator.java
@@ -0,0 +1,54 @@
+package org.codedifferently;
+
+import java.util.Random;
+
+public class recieptcalculator {
+
+    private int itemid;
+    private double itemprice;
+    private double taxrate;
+    private double tax;
+    private double item2price;
+    private double item3price;
+    public boolean rewardmember;
+
+    public recieptcalculator() {
+        Random random = new Random();
+        itemid = random.nextInt(1000) + 100;
+        taxrate = random.nextInt((7)) + 3 /100.0;
+        itemprice = random.nextInt(400) + 19;
+        item2price = random.nextInt(30) +1;
+        item3price = random.nextInt(60) +4;
+
+    }
+
+    public double calculatetax() {
+    return Round((itemprice +item2price + item3price) * taxrate) / 100;
+    }
+    public double calculatetotal(){
+
+        return itemprice + item2price + item3price + calculatetax() + tax;
+    }
+    public String generatethereceipt() {
+        String reciept = "";
+
+        reciept += "Item ID: " + itemid + "\n";
+        reciept += "Price: $" + (itemprice) + "\n";
+        reciept += "Price: $" +  (item2price) + "\n";
+        reciept += "Price: $" +  (item3price) + "\n";
+        reciept += "Tax rate:" +  (taxrate )  + "\n";
+        reciept += "Tax:" + calculatetax() +  "\n";
+
+        reciept += "Total: $" + calculatetotal() + "\n";
+        reciept += "Total (Rounded):" + Round(calculatetotal()) + "\n";
+        return reciept;
+
+    }
+    private double Round(double valuerounded) {
+      return Math.round(valuerounded * 100) / 100;
+    }
+
+
+}
+
+
